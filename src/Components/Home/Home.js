fee514308586c23b3ca5d431edebb10d75b11d5b
shawavisek35 <shawavisek35@gmail.com>
2020-03-22 12:30:23 +0530

    datebase added for users -Avisek

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,7 +11,9 @@ class Home extends Component {
       return (
         <div className="mt-3 bg-light vh-100 container rounded">
           <h1 className="p-5 text-center">Welcome - {this.props.user.email}</h1>
+      
         </div>
+        
       );
     } else {
       return (
